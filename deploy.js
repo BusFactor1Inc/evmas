d7ff68509e546722631168f24791aca377230231
Burton Samograd <BusFactor1@icloud.com>
2018-11-11 15:48:31 -0700

    Added deploy contract scripts.

---
+++ deploy.js
@@ -0,0 +1,68 @@
+const fs = require('fs');
+const Web3 = require('web3');
+
+var address = process.argv[2];
+var abiFile = process.argv[3];
+
+if(!address || !abiFile) {
+	console.log("Usage: cat bytecode | node deploy.js <address> <abiFile>");
+	process.exit(1);
+}
+	
+password = "password";	
+bytecode = undefined;
+
+abi = fs.readFileSync(abiFile, 'UTF8');	
+
+// For localhost Ganache
+const web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:7545"));
+
+process.stdin.on('data', function(chunk) {
+	if(chunk) {
+		bytecode = chunk.toString().split('\n')[0];
+	}
+});
+
+var gasPrice = 1;
+var gas = 0;
+var contractInstance;
+	
+process.stdin.on('end', function() {
+	console.error("Using address: " + address);	
+	console.error("Using password: " + password);	
+	console.error("Using abiFile: " + abiFile);	
+	console.error("Using bytecode: " + bytecode);	
+
+	
+	web3.eth.personal.unlockAccount(address, password).
+	    then(() => { console.log('Account unlocked.'); }).
+	    catch(console.error);
+
+	web3.eth.getGasPrice().
+		then((averageGasPrice) => {
+	       		console.log("Average gas price: " + averageGasPrice);
+	       		gasPrice = averageGasPrice;
+	   }).
+	
+	catch(console.error);
+	let contract = new web3.eth.Contract(JSON.parse(abi), null, { 
+		  	        	     data: '0x' + bytecode 
+	});
+
+	contract.deploy().estimateGas().
+		then((estimatedGas) => {
+        		console.log("Estimated gas: " + estimatedGas);
+	        	gas = estimatedGas;
+	}).
+
+	catch(console.error);
+	contract.deploy().send({from: address,
+				gasPrice: gasPrice, 
+				gas: gas
+	}).then((instance) => { 
+	    console.log("Contract mined at " + instance.options.address);
+	    contractInstance = instance; 
+	});
+});
+
+
