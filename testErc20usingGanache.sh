bb86d185e0e84ee684ffff6ba81b8a004f6656bd
Burton Samograd <BusFactor1@icloud.com>
2018-11-13 11:59:52 -0700

    Update test script.

--- testErc20usingGanache.sh
+++ testErc20usingGanache.sh
@@ -1,3 +1,6 @@
-contract=$(./deploy.sh 0xdd07385605770bB40cB39CC90c32068a6a2bd383 examples/erc20/abi.json < examples/erc20/erc20.evm)
+account=0xdd07385605770bB40cB39CC90c32068a6a2bd383
+destAccount=0x43D6D0611b9C065Ffa2b1038F599B912DCD35139
 
-node examples/erc20/testErc20.js 0xdd07385605770bB40cB39CC90c32068a6a2bd383 $contract examples/erc20/abi.json
+contract=$(./deploy.sh $account examples/erc20/abi.json < examples/erc20/erc20.evm)
+
+node examples/erc20/testErc20.js $account $destAccount $contract examples/erc20/abi.json
